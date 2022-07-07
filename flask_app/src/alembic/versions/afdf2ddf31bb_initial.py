64a4d6b2a52f988965a960c7bb9312df654e2dc5
NatalyaLaktyushkina <tigerclow@mail.ru>
2022-07-07 19:05:34 +0300

    alembic migrations

---
+++ flask_app/src/alembic/versions/afdf2ddf31bb_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: afdf2ddf31bb
+Revises: 
+Create Date: 2022-07-07 01:00:45.331689
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'afdf2ddf31bb'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
